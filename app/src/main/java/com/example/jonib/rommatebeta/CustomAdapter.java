4337e297a094290d128993e5ef06911462635b5c
GitJonibek <jonibeknorboev@gmail.com>
2018-02-12 09:52:48 -0800

    Material design, Responsive Fragments, RecyclerView, New Activity

--- app/src/main/java/com/example/jonib/rommatebeta/CustomAdapter.java
+++ app/src/main/java/com/example/jonib/rommatebeta/CustomAdapter.java
@@ -1,17 +1,13 @@
 package com.example.jonib.rommatebeta;
 
 import java.util.ArrayList;
-import java.util.List;
-
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
--- app/src/main/java/com/example/jonib/rommatebeta/MainActivity.java
+++ app/src/main/java/com/example/jonib/rommatebeta/MainActivity.java
@@ -2,13 +2,9 @@ package com.example.jonib.rommatebeta;
 
 import android.annotation.SuppressLint;
 import android.support.design.widget.TabLayout;
-import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-
 public class MainActivity extends AppCompatActivity {
 
     @SuppressLint("WrongConstant")
